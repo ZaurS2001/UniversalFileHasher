eb815bc50eb29091ababed10f5e3bc4298b0b20b
Zaur Suleimanov <115877946+ZaurS2001@users.noreply.github.com>
2026-01-19 15:40:44 +0400

    Update hasher.py
    Consolidated code formatting

--- hasher.py
+++ hasher.py
@@ -5,7 +5,7 @@ from tkinter import filedialog
 
 def get_file_path():
     root = tk.Tk()
-    root.withdraw()  # Hide the main tkinter window
+    root.withdraw()
     root.attributes('-topmost', True)
     
     file_path = filedialog.askopenfilename(
@@ -36,8 +36,8 @@ def generate_all_hashes(file_path):
             with open(file_path, "rb") as f:
                 for chunk in iter(lambda: f.read(4096), b""):
                     hash_obj.update(chunk)
-            
             print(f"{alg.upper():<15}: {hash_obj.hexdigest()}")
+            
         except (ValueError, TypeError):
             continue
     input("\nPress any key to quit the program...")
